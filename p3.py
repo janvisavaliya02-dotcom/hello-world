590eb846ef25a09e9418ffa29b94fcb0e3603a04
Janvi Savaliya <janvisavaliya02@gmail.com>
2025-08-22 06:02:48 -0700

    Create p3.py

---
+++ p3.py
@@ -0,0 +1,9 @@
+tp1-(1, 2,3, 4,5)
+1st=list(tp1)
+1st.append(100)tp1=tuple(1st)print(tp1)
+list2-(11, 22, 11, 22, 33, 33)
+setl=set(list2)
+print(set1)
+dict1=f
+dict1=dict1.fromkeys(list2, 0)
+print(dict1)
